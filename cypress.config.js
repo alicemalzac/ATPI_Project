a465cca36b09f57520bd25058dba29dcd70362a8
Alice <alicelinsc.malzac@gmail.com>
2022-09-13 09:57:38 -0300

    upload

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  videoUploadOnPasses: true,
   e2e: {
+    baseUrl: 'https://demo.realworld.io' ,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
