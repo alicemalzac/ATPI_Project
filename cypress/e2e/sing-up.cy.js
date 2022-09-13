36b06109dc32e4f35f8415eee9e4fcc76cdf57c8
Alice <alicelinsc.malzac@gmail.com>
2022-09-13 09:57:19 -0300

    test cases upload

--- cypress/e2e/sing-up.cy.js
+++ cypress/e2e/sing-up.cy.js
@@ -1,40 +1,45 @@
+
 describe('Test cases of Sing up/ Register', () => {
   beforeEach(() => {
-   cy.visit('https://demo.realworld.io/#/')
+   cy.visit('/')
+   cy.generateData()
   });
   it('Successfully register', () => {
     //Arrange
-    const faker = require('faker-br');
-    const username = faker.internet.userName()
-    const email = faker.internet.email()
-    const password = faker.internet.password()
+    cy.intercept({
+      method: 'POST',
+      hostname: 'api.realworld.io',
+      path: '/api/users',
+    }).as('postUsers')
 
-    //Act
-    cy.get('[ui-sref="app.register"]')
-      .should('be.visible') //assert
-      .click();
-    cy.get('[placeholder="Username"]')
-      .should('be.visible') //assert
-      .type(username);
-    cy.get('[placeholder="Email"]')
-      .should('be.visible') //assert
-      .type(email);
-    cy.get('[placeholder="Password"]')
-      .should('be.visible') //assert
-      .type(password);
-    cy.get("form[ng-submit]").submit();
+    cy.fixture('data.json').then((user) => {
+      cy.get('[ui-sref="app.register"]')
+        .should('be.visible') //assert
+        .click();
+      cy.get('[placeholder="Username"]')
+        .should('be.visible') //assert
+        .type(user.username);
+      cy.get('[placeholder="Email"]')
+        .should('be.visible') //assert
+        .type(user.email);
+      cy.get('[placeholder="Password"]')
+        .should('be.visible') //assert
+        .type(user.password);  
+      cy.get("form[ng-submit]").submit();
 
     //Assert
-    cy.contains('Your Feed').should('be.visible')
-    cy.get(':nth-child(4) > .nav-link').should('contain', username)
-  })
-  it('Username field cant be empty', () => {
-    //Arrange
-    const faker = require('faker-br');
-    const email = faker.internet.email()
-    const password = faker.internet.password()
+      cy.contains('Your Feed').should('be.visible')
+      cy.get(':nth-child(4) > .nav-link').should('contain', user.username)
+    })
 
+    cy.wait('@postUsers').then( interceptions => {
+      expect(interceptions.response.statusCode).to.be.eq(200)
+      expect(interceptions.response.body.user.token).to.not.be.empty
+    })
+  })  
+  it('Username field cant be empty', () => {
     //Act
+    cy.fixture('data.json').then((user) => {
     cy.get('[ui-sref="app.register"]')
       .should('be.visible') //assert
       .click();
@@ -42,59 +47,109 @@ describe('Test cases of Sing up/ Register', () => {
       .should('be.visible') //assert
     cy.get('[placeholder="Email"]')
       .should('be.visible') //assert
-      .type(email);
+      .type(user.email);
     cy.get('[placeholder="Password"]')
       .should('be.visible') //assert
-      .type(password);
+      .type(user.password);
     cy.get("form[ng-submit]").submit();
-
+    })
     //Assert
     cy.contains("username can't be blank").should('be.visible')
   })
   it('Email field cant be empty', () => {
-    //Arrange
-    const faker = require('faker-br');
-    const username = faker.internet.userName()
-    const password = faker.internet.password()
 
     //Act
+    cy.fixture('data.json').then((user) => {
     cy.get('[ui-sref="app.register"]')
       .should('be.visible') //assert
       .click();
     cy.get('[placeholder="Username"]')
       .should('be.visible') //assert
-      .type(username);
+      .type(user.username);
     cy.get('[placeholder="Email"]')
       .should('be.visible') //assert
     cy.get('[placeholder="Password"]')
       .should('be.visible') //assert
-      .type(password);
+      .type(user.password);
     cy.get("form[ng-submit]").submit();
-
+    })
     //Assert
     cy.contains("email can't be blank").should('be.visible')
   })
   it('Password field cant be empty', () => {
-    //Arrange
-    const faker = require('faker-br');
-    const username = faker.internet.userName()
-    const email = faker.internet.email()
 
-    //Act
+    cy.fixture('data.json').then((user) => {
     cy.get('[ui-sref="app.register"]')
       .should('be.visible') //assert
       .click();
     cy.get('[placeholder="Username"]')
       .should('be.visible') //assert
-      .type(username);
+      .type(user.username);
     cy.get('[placeholder="Email"]')
       .should('be.visible') //assert
-      .type(email);
+      .type(user.email);
     cy.get('[placeholder="Password"]')
       .should('be.visible') //assert
     cy.get("form[ng-submit]").submit();
-
+    })
     //Assert
     cy.contains("password can't be blank").should('be.visible')
+  })
+  it('Username already been taken', () => {
+    cy.fixture('data.json').then((user) => {
+    cy.get('[ui-sref="app.register"]')
+      .should('be.visible') //assert
+      .click();
+    cy.get('[placeholder="Username"]')
+      .should('be.visible') //assert
+      .type("aaaa");
+    cy.get('[placeholder="Email"]')
+      .should('be.visible') //assert
+      .type(user.email);
+      cy.get('[placeholder="Password"]')
+      .should('be.visible') //assert
+      .type(user.password);
+    cy.get("form[ng-submit]").submit();
+    })
+    //Assert
+    cy.contains("username has already been taken").should('be.visible')
+  })
+  it('Email already been taken', () => {
+    cy.fixture('data.json').then((user) => {
+    cy.get('[ui-sref="app.register"]')
+      .should('be.visible') //assert
+      .click();
+    cy.get('[placeholder="Username"]')
+      .should('be.visible') //assert
+      .type(user.username);
+    cy.get('[placeholder="Email"]')
+      .should('be.visible') //assert
+      .type("aaaa@Aaaa.com");
+      cy.get('[placeholder="Password"]')
+      .should('be.visible') //assert
+      .type(user.password);
+    cy.get("form[ng-submit]").submit();
+    })
+    //Assert
+    cy.contains("email has already been taken").should('be.visible')
+  })
+  it('Email validation', () => {
+    cy.fixture('data.json').then((user) => {
+    cy.get('[ui-sref="app.register"]')
+      .should('be.visible') //assert
+      .click();
+    cy.get('[placeholder="Username"]')
+      .should('be.visible') //assert
+      .type(user.username);
+    cy.get('[placeholder="Email"]')
+      .should('be.visible') //assert
+      .type("aaaa@Aaaa.com");
+      cy.get('[placeholder="Password"]')
+      .should('be.visible') //assert
+      .type(user.password);
+    cy.get("form[ng-submit]").submit();
+    })
+    //Assert
+    cy.contains("email has already been taken").should('be.visible')
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,11 +1,8 @@
 Cypress.Commands.add('generateData', () => {
-    const faker = require('faker-br');
-  
-    cy.writeFile('cypress/fixtures/data.json', () => {
-        return {
-          'username':`${faker.internet.userName()}`,
-          'email':`${faker.internet.email()}`,
-          'password':`${faker.internet.password()}`,
-        }
+  const faker = require('faker-br');
+    cy.writeFile('cypress/fixtures/data.json', {
+      'username':`${faker.internet.userName()}`,
+      'email':`${faker.internet.email()}`,
+      'password':`${faker.internet.password()}`
     })
   })
